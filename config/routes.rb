4b76877dfdd1bb22f2e2ff9c3d3d04f27551780a
Ryan Flores <rfloper@icloud.com>
2018-03-21 20:27:59 +0100

    Add subdomain constraint

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,4 @@
 Rails.application.routes.draw do
-  # namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-  #   post 'auth' => 'authentication#create'
-  # end
-
   constraints subdomain: 'api' do
     scope module: :api do
       namespace :v1 do
